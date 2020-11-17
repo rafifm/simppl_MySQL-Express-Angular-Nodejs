786375692306aa6b36c356560528615da64faf0d
rafif <rafif.musyaffa24@gmail.com>
2020-11-17 22:50:36 +0700

    pindah server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const debug = require("debug")("node-angular");
-const app = require("./app");
+const app = require("./backend/app");
 
 const normalizePort = val => {
   var port = parseInt(val, 10);
