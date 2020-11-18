5eab33ce92ec59ba6bfd22d0be490683cd4600a2
rafif <rafif.musyaffa24@gmail.com>
2020-11-18 19:36:04 +0700

    percobaan deploy 7

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("backend/images")));
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/dist"));
 app.use((req, res, next) => {
-  res.sendFile(__dirname + "../src/index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 })
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
