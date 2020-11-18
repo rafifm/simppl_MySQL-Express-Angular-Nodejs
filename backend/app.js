3408b1646ebdb9da58cd592158b61682d068ed7e
rafif <rafif.musyaffa24@gmail.com>
2020-11-18 16:13:14 +0700

    update deploy

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("backend/images")));
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(express.static(__dirname + "/dist/"));
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
