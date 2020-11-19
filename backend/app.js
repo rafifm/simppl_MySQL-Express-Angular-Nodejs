a82c1e88483c99b6f73fe679bd1da7958d97a4dd
rafif <rafif.musyaffa24@gmail.com>
2020-11-19 19:54:15 +0700

    deploy 31

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(app.get('port'));
-
-app.set('views', __dirname + '/dist');
-app.set('view engine', 'jade');
+s
 app.use("/",express.static(path.join(__dirname,"dist")));
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname,"dist","index.html"));
