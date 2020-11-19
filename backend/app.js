238debde7fd83245372c643ba2d464bdab90a42b
rafif <rafif.musyaffa24@gmail.com>
2020-11-19 19:38:07 +0700

    deploy 32

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(app.get('port'));
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/dist');
 app.set('view engine', 'jade');
-app.use("/",express.static(path.join(__dirname,"backend","dist")));
+app.use("/",express.static(path.join(__dirname,"dist")));
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname,"backend","dist","index.html"));
+  res.sendFile(path.join(__dirname,"dist","index.html"));
 });
 
 // app.use((req, res, next) => {
