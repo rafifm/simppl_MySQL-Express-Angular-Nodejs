96a1989779e7ba4a566479cf6ac0f332abfcabab
rafif <rafif.musyaffa24@gmail.com>
2020-11-19 15:33:05 +0700

    deploy 22

--- src/app/layouts/akun/_services/auth.service.ts
+++ src/app/layouts/akun/_services/auth.service.ts
@@ -1,26 +1,28 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
-const AUTH_API = 'http://localhost:4000/api/auth/';
+
 const httpOptions = {headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private AUTH_API = environment.API_URL;
 
   constructor(private http: HttpClient) { }
 
   login(rahasia): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(this.AUTH_API + 'signin', {
       email_pengguna: rahasia.email,
       password_pengguna: rahasia.password
     }, httpOptions);
   }
 
   register(pengguna): Observable<any> {
-    return this.http.post(AUTH_API + 'signup', {
+    return this.http.post(this.AUTH_API + 'signup', {
       email_pengguna: pengguna.email,
       password_pengguna: pengguna.password
     }, httpOptions);
