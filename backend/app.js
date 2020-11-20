74993e0976aa60e45a6b4d8688dd97f1b9045127
rafif <rafif.musyaffa24@gmail.com>
2020-11-20 09:53:51 +0700

    deploy 41

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(app.get('port'));
-app.use(express.static(path.join(__dirname,'dist')));
-app.use((req, res, next) => {
+app.use('/',express.static(path.join(__dirname,'dist')));
+app.get('*',(req, res, next) => {
   res.sendFile(path.join(__dirname,'dist','index.html'));
 });
 
