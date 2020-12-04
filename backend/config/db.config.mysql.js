cfe324004e6f387587fce9e1956a141dfec23619
rafif <rafif.musyaffa24@gmail.com>
2020-12-04 21:01:31 +0700

    tambah staff edit delete dosen config

--- backend/config/db.config.mysql.js
+++ backend/config/db.config.mysql.js
@@ -1,22 +1,8 @@
-// module.exports = {
-//   HOST: "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   USER: "d5532t59ip5y4u5u",
-//   PASSWORD: "zl1zuv2e6lwivcg5",
-//   DB: "llqr4lbz0uxmfuvf",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "simppl",
+  HOST: "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  USER: "d5532t59ip5y4u5u",
+  PASSWORD: "zl1zuv2e6lwivcg5",
+  DB: "llqr4lbz0uxmfuvf",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -25,3 +11,17 @@ module.exports = {
     idle: 10000
   }
 };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "simppl",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
